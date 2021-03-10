8a1f33e02776696608044b1fdde9605f4f11714a
Nitansh <nitansh11@gmail.com>
2021-03-10 10:40:49 +0530

    added some filters

--- back-end-redbus/app.js
+++ back-end-redbus/app.js
@@ -1,3 +1,21 @@
 const express = require("express");
 const app = express();
-app.listen(8000, () => console.log("Listening at Port 8000"));
+const mongoose = require("mongoose");
+
+const connect = () => {
+  return mongoose.connect(
+    "mongodb+srv://redbus_db_user_1:umJkhSujb8dZoc2a@redbuscnstructweek.bujg6.mongodb.net/redbus?retryWrites=true&w=majority",
+    {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    }
+  );
+};
+
+const start = async () => {
+  await connect();
+  app.listen(8000, () => console.log("Listening at Port 8000"));
+};
+start();
--- front-end-redbus/src/Components/SelectBus/Left.jsx
+++ front-end-redbus/src/Components/SelectBus/Left.jsx
@@ -1,7 +1,113 @@
 import React from "react";
 import styles from "./Left.module.css";
+
 const Left = () => {
-  return <div className={styles.Left}>Left</div>;
+  return (
+    <div className={styles.Left}>
+      {/* 1. Basic Filters */}
+      <div className={styles.Left_filters}>
+        <p>FILTERS</p>
+        <ul>
+          <li>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            Live Tracking (32)
+          </li>
+
+          <li>
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            Reschedulable (28)
+          </li>
+        </ul>
+      </div>
+      {/* 2. Departure Filters */}
+      <div className={styles.Left_filters}>
+        <p>DEPARTURE TIME</p>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+        </ul>
+      </div>
+      {/* 3. Bus Types Filters */}
+      <div className={styles.Left_filters}>
+        <p>BUS TYPES</p>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>SEATER (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>SLEEPER (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>AC (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>NONAC (0)</p>
+          </li>
+        </ul>
+      </div>
+      {/* 4. Arrival Time Filters */}
+
+      <div className={styles.Left_filters}>
+        <p>ARRIVAL TIME</p>
+        <ul>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+          <li>
+            <input type="checkbox" />
+            <i class="fa fa-map-marker" aria-hidden="true"></i>
+            <p>Before 6 am (0)</p>
+          </li>
+        </ul>
+      </div>
+      {/* 5. Boarding Point Filters */}
+      <div className={styles.Left_filters__search}>
+        <i class="fa fa-search" aria-hidden="true"></i>
+        <input type="text" />
+      </div>
+      {/* 6. Dropping Point Filters */}
+      {/* 7. Operator */}
+      {/* 8. Amenities */}
+    </div>
+  );
 };
 
 export default Left;
