00d75f0bbf4a25ef51cbaabc4294e109deadc214
Nitansh <nitansh11@gmail.com>
2021-03-11 11:02:09 +0530

    added customers to db

--- back-end-redbus/routes/customer.js
+++ back-end-redbus/routes/customer.js
@@ -2,4 +2,6 @@ const express = require("express");
 const router = express.Router();
 const customerController = require("../controllers/customer");
 
+router.post("/v1/api/customers", customerController.addNewCustomer);
+
 module.exports = router;
