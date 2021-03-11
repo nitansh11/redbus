eeff5d3075e308d7cf74b0c9dbdba2ca331be8b2
Nitansh <nitansh11@gmail.com>
2021-03-11 09:22:44 +0530

    added google auth

--- front-end-redbus/src/Components/Navbar/Navbar.jsx
+++ front-end-redbus/src/Components/Navbar/Navbar.jsx
@@ -5,10 +5,26 @@ import MenuItem from "@material-ui/core/MenuItem";
 import { RiArrowDropDownLine } from "react-icons/ri";
 import { MdAccountCircle } from "react-icons/md";
 import { Link } from "react-router-dom";
+import { GoogleLogin } from "react-google-login";
+import { useSelector, useDispatch } from "react-redux";
+import { loginSuccess, loginFailure, logout } from "../../Redux/auth/actions";
+import { useHistory } from "react-router-dom";
 const Navbar = () => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [anchorEl2, setAnchorEl2] = React.useState(null);
+  const dispatch = useDispatch();
 
+  const isLoggedIn = useSelector((state) => state.authReducer.isLoggedIn);
+  const currentCustomer = useSelector(
+    (state) => state.authReducer.currentCustomer
+  );
+
+  console.log("Here: ", isLoggedIn, currentCustomer);
+  const history = useHistory();
+  const handleLogout = () => {
+    dispatch(logout());
+    history.push("/");
+  };
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -22,6 +38,7 @@ const Navbar = () => {
   const handleClose2 = () => {
     setAnchorEl2(null);
   };
+
   return (
     <div className={styles.Navbar}>
       <div className={styles.leftSide_header}>
@@ -39,47 +56,81 @@ const Navbar = () => {
       </div>
 
       <ul className={styles.Navbar__listTwo}>
-        <div className = {styles.rightSide_header}>
-        <li>Help</li>
-        <li>Manage Booking</li>
-        <li>
-        <div>
-          <RiArrowDropDownLine className = {styles.icons} onClick={handleClick}/>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              keepMounted
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Bus Ticket</MenuItem>
-              <MenuItem onClick={handleClose}>Cancel</MenuItem>
-              <MenuItem onClick={handleClose}>Reschedule</MenuItem>
-              <MenuItem onClick={handleClose}>Show My Ticket</MenuItem>
-              <MenuItem onClick={handleClose}>Email / SMS</MenuItem>
-            </Menu>
-        </div>
-        </li>
-        <li><MdAccountCircle className = {styles.icons} style = {{fontSize : "30px"}}/></li>
-        <li>
-        <div>
-          <RiArrowDropDownLine className = {styles.icons} onClick={handleClick2}/>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl2}
-              keepMounted
-              open={Boolean(anchorEl2)}
-              onClose={handleClose2}
-            >
-              <MenuItem onClick={handleClose2}>My Trips</MenuItem>
-              <MenuItem onClick={handleClose2}>Wallet/Cards</MenuItem>
-              <MenuItem onClick={handleClose2}>My Profile</MenuItem>
-              <MenuItem onClick={handleClose2}>Wallet</MenuItem>
-              <MenuItem onClick={handleClose2}>SignOut</MenuItem>
-              <MenuItem onClick={handleClose2}>SignOut from all devices</MenuItem>
-            </Menu>
-        </div>
-        </li>
+        <div className={styles.rightSide_header}>
+          <li>Help</li>
+          <li>Manage Booking</li>
+          <li>
+            <div>
+              <RiArrowDropDownLine
+                className={styles.icons}
+                onClick={handleClick}
+              />
+              <Menu
+                id="simple-menu"
+                anchorEl={anchorEl}
+                keepMounted
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Bus Ticket</MenuItem>
+                <MenuItem onClick={handleClose}>Cancel</MenuItem>
+                <MenuItem onClick={handleClose}>Reschedule</MenuItem>
+                <MenuItem onClick={handleClose}>Show My Ticket</MenuItem>
+                <MenuItem onClick={handleClose}>Email / SMS</MenuItem>
+              </Menu>
+            </div>
+          </li>
+          <li>
+            <MdAccountCircle
+              className={styles.icons}
+              style={{ fontSize: "30px" }}
+            />
+          </li>
+          <li>
+            <div>
+              <RiArrowDropDownLine
+                className={styles.icons}
+                onClick={handleClick2}
+              />
+
+              {isLoggedIn && currentCustomer ? (
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl2}
+                  keepMounted
+                  open={Boolean(anchorEl2)}
+                  onClose={handleClose2}
+                >
+                  <MenuItem onClick={handleClose2}>My Trips</MenuItem>
+                  <MenuItem onClick={handleClose2}>Wallet/Cards</MenuItem>
+                  <MenuItem onClick={handleClose2}>My Profile</MenuItem>
+                  <MenuItem onClick={handleClose2}>Wallet</MenuItem>
+                  <MenuItem onClick={handleLogout}>SignOut</MenuItem>
+                </Menu>
+              ) : (
+                <Menu
+                  id="simple-menu"
+                  anchorEl={anchorEl2}
+                  keepMounted
+                  open={Boolean(anchorEl2)}
+                  onClose={handleClose2}
+                >
+                  <MenuItem onClick={handleClose2}>
+                    <GoogleLogin
+                      clientId="493530183469-naj3i844vuh8ru5usav057k5kuabc3iq.apps.googleusercontent.com"
+                      onSuccess={(response) => {
+                        dispatch(loginSuccess(response));
+                        console.log(response);
+                      }}
+                      onFailure={(response) => {
+                        dispatch(loginFailure(response));
+                      }}
+                    />
+                  </MenuItem>
+                </Menu>
+              )}
+            </div>
+          </li>
         </div>
       </ul>
     </div>
