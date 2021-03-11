1204cfee11567da8ff0c879696a15165fa761a97
udit22022000 <ping2udit@gmail.com>
2021-03-11 08:27:05 +0530

    added sorting bar

--- front-end-redbus/src/Components/SelectBus/Right.jsx
+++ front-end-redbus/src/Components/SelectBus/Right.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./Right.module.css";
 import { BusBox } from "../BusDetails/BusBox/BusBox";
+import { SortingBar } from "../BusDetails/SortingBar/SortingBar";
 const Right = () => {
   return (
     <div className={styles.Right}>
+      <SortingBar />
       <BusBox />
       <BusBox />
       <BusBox />
