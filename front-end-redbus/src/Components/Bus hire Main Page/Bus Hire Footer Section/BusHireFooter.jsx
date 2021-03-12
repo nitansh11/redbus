9a0b22837a42285eb93942264f0d87613177b454
archana singh <archanasingh1643@gmail.com>
2021-03-12 09:43:36 +0530

    added sub footer

---
+++ front-end-redbus/src/Components/Bus hire Main Page/Bus Hire Footer Section/BusHireFooter.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import Styles from './busHireFooter.module.css'
+
+const BusHireFooter = () => {
+    return (
+        <div>
+            <div className = {Styles.busHire_Footer}>
+                {/* <div className = {Styles..busHireFooterTextSection}> */}
+
+                {/* </div> */}
+            </div>
+        </div>
+    )
+}
+
+export default BusHireFooter
